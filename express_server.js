beb72c8794e3c289fe994a23291caad750bfd835
Idil Mahamed <mahamed.idil@gmail.com>
2023-10-23 23:34:52 -0400

    New /urls/:id Routes

--- express_server.js
+++ express_server.js
@@ -25,6 +25,13 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id
+  const templateVars = { id:id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
